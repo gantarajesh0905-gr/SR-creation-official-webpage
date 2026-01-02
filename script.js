bcb461872ea30db52e4fea46198c2b9e498d5a70
gantarajesh0905-gr <gantarajesh0905@gmail.com>
2026-01-02 11:19:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function contactNow() {
     alert("Contact us via WhatsApp or Email to start your project!");
 }
+
+<script>
+function contactNow() {
+    document.getElementById("contact").scrollIntoView({
+        behavior: "smooth"
+    });
+}
+</script>
